919bc81805fbc5550f0dbf13f93accbab79cec57
LAPTOP-3KA930G9\baschatt <baschattkrab@gmail.com>
2020-09-02 11:48:27 +0700

    store TimeCalculation.normalTime in String "time" to easy for read

--- src/seminar/AgendaDisplay.java
+++ src/seminar/AgendaDisplay.java
@@ -5,8 +5,9 @@ import java.util.Map;
 public class AgendaDisplay {
 	public static void displaySeminar(Map<String, Integer> sortedSeminar) {
 		
-		for (Map.Entry<String, Integer> seminar : sortedSeminar.entrySet()) { 
-            System.out.println(TimeCalculation.normalTime(seminar.getValue()) + " " + seminar.getKey() + " " + seminar.getValue() + "min"); 
+		for (Map.Entry<String, Integer> seminar : sortedSeminar.entrySet()) {
+			String time = TimeCalculation.normalTime(seminar.getValue());
+            System.out.println(time + " " + seminar.getKey() + " " + seminar.getValue() + "min"); 
         } 
 	}
 }
